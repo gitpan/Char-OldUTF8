5b41a69b54aabcc8ad23e2c4963ff0ad8963ef34
INABA Hitoshi <ina@cpan.org>
2014-09-07 16:40:52 +0000

    Import of INA/Char-OldUTF8-1.01 from CPAN.
    gitpan-cpan-distribution: Char-OldUTF8
    gitpan-cpan-version:      1.01
    gitpan-cpan-path:         INA/Char/OldUTF8/Char-OldUTF8-1.01.tar.gz
    gitpan-cpan-author:       INA
    gitpan-cpan-maturity:     released

--- OldUTF8.pm
+++ OldUTF8.pm
@@ -17,7 +17,7 @@ use 5.00503;    # Galapagos Consensus 1998 for primetools
 # (and so on)
 
 BEGIN { eval q{ use vars qw($VERSION) } }
-$VERSION = sprintf '%d.%02d', q$Revision: 1.00 $ =~ /(\d+)/oxmsg;
+$VERSION = sprintf '%d.%02d', q$Revision: 1.01 $ =~ /(\d+)/oxmsg;
 
 BEGIN {
     if ($^X =~ / jperl /oxmsi) {
@@ -521,7 +521,9 @@ sub OldUTF8::escape_script {
         # in Chapter 5: Pattern Matching
         # of ISBN 978-0-596-00492-7 Programming Perl 4th Edition.
 
-        $e_script .= sprintf("use Eoldutf8 %s.0;\n", $OldUTF8::VERSION); # require run-time routines version
+        # '...' quote to avoid "Octal number in vector unsupported" on perl 5.6
+
+        $e_script .= sprintf("use Eoldutf8 '%s.0'; # 'quote' for perl5.6\n", $OldUTF8::VERSION); # require run-time routines version
 
         # use OldUTF8 version qw(ord reverse getc);
         $function_ord     = 'ord';
